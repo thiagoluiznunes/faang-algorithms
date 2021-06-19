0d69315240457ad7e9e59bbc0fe47a1df0160774
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2021-06-19 16:53:34 -0300

    Create graph functions

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
-import "faang-algorithms/list"
+import (
+	"faang-algorithms/graph"
+)
 
 func main() {
-	var head list.Node
-	list.InsertFirst("Thiago", &head)
-	list.InsertFirst("Luiz", &head)
-	list.InsertFirst("Pereira", &head)
-	list.InsertFirst("Nunes", &head)
-	list.PrintList(&head)
 
-	list.RemoveFirst(&head)
-	list.RemoveFirst(&head)
-	list.RemoveFirst(&head)
-	list.RemoveFirst(&head)
-	list.PrintList(&head)
+	// List execution test
+	// var head list.Node
+	// list.InsertFirst("Thiago", &head)
+	// list.PrintList(&head)
+	// list.RemoveFirst(&head)
+	// list.PrintList(&head)
+
+	var myGraph graph.Graph
+	firstVertex := graph.Vertex{
+		Value: "Thiago Luiz",
+		Edges: nil,
+	}
+	myGraph.InsertVertex(&firstVertex)
+
+	secondVertex := graph.Vertex{
+		Value: "Debora Santos",
+		Edges: nil,
+	}
+	secondVertex.Edges = append(secondVertex.Edges, &firstVertex)
+	myGraph.InsertVertex(&secondVertex)
+
+	thirdVertex := graph.Vertex{
+		Value: "Marta Santos",
+		Edges: nil,
+	}
+	thirdVertex.Edges = append(thirdVertex.Edges, &firstVertex)
+	thirdVertex.Edges = append(thirdVertex.Edges, &secondVertex)
+	myGraph.InsertVertex(&thirdVertex)
+
+	myGraph.PrintGraph()
 }
