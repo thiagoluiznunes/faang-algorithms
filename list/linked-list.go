e930f7c37fdac8580a4d6c379c7f192a4222c466
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2021-06-19 12:27:46 -0300

    Create addNode function

--- list/linked-list.go
+++ list/linked-list.go
@@ -1,13 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Node struct {
 	Value string
 	Next  *Node
 }
 
-func (n *Node) addNode() error {
+func addNode(value string, node *Node) error {
+
+	if node == nil {
+		return nil
+	} else if node.Next != nil {
+		addNode(value, node.Next)
+	} else {
+		node.Next = &Node{
+			Value: value,
+			Next:  nil,
+		}
+	}
+
+	return nil
+}
+
+func printList(node *Node) error {
+
+	if node == nil {
+		return nil
+	} else if node.Next != nil {
+		printList(node.Next)
+	}
+	fmt.Println(node.Value)
+
 	return nil
 }
 
@@ -16,5 +42,11 @@ func (n *Node) removeNode() error {
 }
 
 func main() {
-	fmt.Println("Teste go mod")
+
+	rootNode := Node{}
+	addNode("Thiago", &rootNode)
+	addNode("Luiz", &rootNode)
+	addNode("Pereira", &rootNode)
+	addNode("Nunes", &rootNode)
+	printList(&rootNode)
 }
